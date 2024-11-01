de3bd38a112fa4a6da63acd3443bbcf729e5bc11
Dan Laugharn <laugharn@gmail.com>
2024-11-01 09:10:34 -0500

    ♻️ Refactors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,7 @@ const config = [
   {
     rules: {
       '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
       'import/no-anonymous-default-export': 'off',
       'sort-imports': [
         'warn',
--- lib/util/index.ts
+++ lib/util/index.ts
@@ -1,14 +1,14 @@
-export function chunk(arr: any[], size: number) {
-  return Array.from({ length: Math.ceil(arr.length / size) }, (_: any, i: number) =>
-    arr.slice(i * size, i * size + size),
-  )
+import type { Any } from '@/types'
+
+export function chunk(arr: Any[], size: number) {
+  return Array.from({ length: Math.ceil(arr.length / size) }, (_, i: number) => arr.slice(i * size, i * size + size))
 }
 
 export function ensureStartsWith(stringToCheck: string, startsWith: string) {
   return stringToCheck.startsWith(startsWith) ? stringToCheck : `${startsWith}${stringToCheck}`
 }
 
-export function intersection(a: any[], b: any[]) {
+export function intersection(a: Any[], b: Any[]) {
   return a.filter((x) => b.includes(x))
 }
 
@@ -16,6 +16,6 @@ export function isObject(object: unknown): object is Record<string, unknown> {
   return typeof object === 'object' && object !== null && !Array.isArray(object)
 }
 
-export function last(arr: any[]) {
+export function last(arr: Any[]) {
   return arr[arr.length - 1]
 }
